9c4873c6a8154b43473257e9f55c9c1dc72456d1
Lingrode <illia050@ukr.net>
2020-12-08 12:22:43 +0200

    added product-page

--- app/js/main.js
+++ app/js/main.js
@@ -1,18 +1,42 @@
 $(function () {
 
+  $('.product-tabs__top-item').on('click', function (e) {
+    e.preventDefault();
+    $('.product-tabs__top-item').removeClass('product-tabs__top-item--active');
+    $(this).addClass('product-tabs__top-item--active');
+
+    $('.product-tabs__content-item').removeClass('product-tabs__content-item--active');
+    $($(this).attr('href')).addClass('product-tabs__content-item--active');
+  });
+
+  $('.product-slide__thumb').slick({
+    asNavFor: '.product-slide__big',
+    focusOnSelect: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    vertical: true,
+    draggable: false
+  })
+  $('.product-slide__big').slick({
+    asNavFor: '.product-slide__thumb',
+    draggable: false,
+    arrows: false,
+    fade: true
+  })
+
   $('.shop-content__filter-btn').on('click', function () {
     $('.shop-content__filter-btn').removeClass('shop-content__filter-btn--active')
     $(this).addClass('shop-content__filter-btn--active')
   })
 
-  $('.button-list').on('click', function() {
+  $('.button-list').on('click', function () {
     $('.product-item').addClass('product-item--list');
   })
-  $('.button-grid').on('click', function() {
+  $('.button-grid').on('click', function () {
     $('.product-item').removeClass('product-item--list');
   })
 
-  $('.select-style').styler()
+  $('.select-style, .product-one__item-num').styler()
 
   $('.filter-price__input').ionRangeSlider({
     type: "double",
@@ -24,7 +48,7 @@ $(function () {
     onChange: function (data) {
       $('.filter-price__from').text(data.from);
       $('.filter-price__to').text(data.to);
-  },
+    },
   });
 
   $('.top-slider__inner').slick({
@@ -55,31 +79,31 @@ $(function () {
       'seconds': seconds
     };
   }
-  
+
   function initializeClock(id, endtime) {
     var clock = document.querySelector('.promo__clock');
     var daysSpan = clock.querySelector('.promo__days');
     var hoursSpan = clock.querySelector('.promo__hours');
     var minutesSpan = clock.querySelector('.promo__minutes');
     var secondsSpan = clock.querySelector('.promo__seconds');
-  
+
     function updateClock() {
       var t = getTimeRemaining(endtime);
-  
+
       daysSpan.innerHTML = t.days;
       hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
       minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
       secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
-  
+
       if (t.total <= 0) {
         clearInterval(timeinterval);
       }
     }
-  
+
     updateClock();
     var timeinterval = setInterval(updateClock, 1000);
   }
-  
+
   var deadline = $('.promo__clock').attr('data-time');
   initializeClock('promo__clock', deadline);
 
